1305bac39b14c51dc988ae268bf869b10970a03f
rooney123777 <rooney123777@gmail.com>
2022-10-29 16:55:50 +0300

    version 1.0.12 release - Added BiometricUtils

---
+++ lightbulb-commons/src/main/java/com/github/RooneyAndShadows/lightbulb/commons/utils/BiometricUtils.kt
@@ -0,0 +1,201 @@
+package com.github.rooneyandshadows.lightbulb.commons.utils
+
+import android.app.admin.DevicePolicyManager
+import android.content.Context
+import android.content.Intent
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.*
+import androidx.biometric.BiometricPrompt
+import androidx.biometric.BiometricPrompt.PromptInfo
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
+
+@Suppress("unused")
+class BiometricUtils {
+    companion object {
+
+        @JvmStatic
+        fun requestBiometricAuthentication(
+            activity: FragmentActivity,
+            settings: AuthSettings
+        ): Companion {
+            buildPrompt(activity, settings).apply {
+                checkBioMetricSupported(activity, this, settings)
+            }
+            return Companion
+        }
+
+        private fun buildPrompt(
+            activity: FragmentActivity,
+            settings: AuthSettings
+        ): BiometricPrompt {
+            val executor = ContextCompat.getMainExecutor(activity)
+            return BiometricPrompt(activity,
+                executor, object : BiometricPrompt.AuthenticationCallback() {
+                    override fun onAuthenticationError(
+                        errorCode: Int,
+                        errString: CharSequence
+                    ) {
+                        super.onAuthenticationError(errorCode, errString)
+                        if (errorCode == BiometricPrompt.ERROR_NO_BIOMETRICS && settings.forceCreateAuthIfNone) {
+                            requestCreateCredentials(activity)
+                        } else {
+                            settings.authenticationListeners?.onAuthenticationError(
+                                errorCode,
+                                errString.toString()
+                            )
+                        }
+                    }
+
+                    override fun onAuthenticationSucceeded(
+                        result: BiometricPrompt.AuthenticationResult
+                    ) {
+                        super.onAuthenticationSucceeded(result)
+                        settings.authenticationListeners?.onAuthenticationSucceeded()
+                    }
+
+                    override fun onAuthenticationFailed() {
+                        super.onAuthenticationFailed()
+                        settings.authenticationListeners?.onAuthenticationFailed()
+                    }
+                })
+        }
+
+        private fun checkBioMetricSupported(
+            context: Context,
+            biometricPrompt: BiometricPrompt,
+            settings: AuthSettings
+        ) {
+            val manager = BiometricManager.from(context)
+            when (manager.canAuthenticate(BIOMETRIC_WEAK or BIOMETRIC_STRONG)) {
+                BiometricManager.BIOMETRIC_SUCCESS -> {
+                    //"App can authenticate using biometrics."
+                    requestFingerPrint(
+                        biometricPrompt,
+                        settings.title,
+                        settings.subTitle,
+                        settings.cancelButtonText
+                    )
+                }
+                BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                    //"No biometric features available on this device."
+                    requestPinOrFigure(
+                        biometricPrompt,
+                        settings.title,
+                        settings.subTitle
+                    )
+                }
+                BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                    //"Biometric features are currently unavailable."
+                    requestPinOrFigure(
+                        biometricPrompt,
+                        settings.title,
+                        settings.subTitle
+                    )
+                }
+                BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                    //"No finger prints found"
+                    requestPinOrFigure(
+                        biometricPrompt,
+                        settings.title,
+                        settings.subTitle
+                    )
+                }
+                else -> {
+                    //"Unknown cause"
+                }
+            }
+        }
+
+        private fun requestFingerPrint(
+            biometricPrompt: BiometricPrompt,
+            title: String,
+            subtitle: String,
+            cancelButtonText: String
+        ) {
+            val dialog = PromptInfo.Builder()
+                .setTitle(title)
+                .setSubtitle(subtitle)
+            val promptInfo: PromptInfo.Builder = dialog
+            promptInfo.setNegativeButtonText(cancelButtonText)
+            biometricPrompt.authenticate(promptInfo.build())
+        }
+
+        private fun requestPinOrFigure(
+            biometricPrompt: BiometricPrompt,
+            title: String,
+            subtitle: String,
+        ) {
+            val dialog = PromptInfo.Builder()
+                .setTitle(title)
+                .setSubtitle(subtitle)
+            val promptInfo: PromptInfo.Builder = dialog
+            promptInfo.setAllowedAuthenticators(BIOMETRIC_WEAK or DEVICE_CREDENTIAL)
+            biometricPrompt.authenticate(promptInfo.build())
+        }
+
+        private fun requestCreateCredentials(context: Context) {
+            // Prompts the user to create credentials that your app accepts.
+            //Open settings to set credential
+            val intent = Intent(DevicePolicyManager.ACTION_SET_NEW_PASSWORD)
+            context.startActivity(intent)
+            /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                // Prompts the user to create credentials that your app accepts.
+                //Open settings to set credential
+                val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL)
+                enrollIntent.putExtra(
+                    Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                    BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL
+                )
+                context.startActivity(enrollIntent)
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                val intent = Intent(Settings.ACTION_FINGERPRINT_ENROLL)
+                context.startActivity(intent)
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val intent = Intent(Settings.ACTION_SECURITY_SETTINGS)
+                context.startActivity(intent)
+            }*/
+        }
+    }
+
+    class AuthSettings {
+        var title: String = "Biometric Auth"
+            private set
+        var subTitle: String = "Please authenticate using your biometric credential"
+            private set
+        var cancelButtonText: String = "Cancel"
+            private set
+        var forceCreateAuthIfNone: Boolean = false
+            private set
+        var authenticationListeners: AuthenticationListeners? = null
+            private set
+
+        fun withTitle(title: String): AuthSettings {
+            return apply { this.title = title }
+        }
+
+        fun withSubTitle(subTitle: String): AuthSettings {
+            return apply { this.subTitle = subTitle }
+        }
+
+        fun withCancelButtonText(cancelButtonText: String): AuthSettings {
+            return apply { this.cancelButtonText = cancelButtonText }
+        }
+
+        fun withForceCreateAuthIfNone(forceCreateAuthIfNone: Boolean): AuthSettings {
+            return apply { this.forceCreateAuthIfNone = forceCreateAuthIfNone }
+        }
+
+        fun withListeners(authenticationListeners: AuthenticationListeners?): AuthSettings {
+            return apply { this.authenticationListeners = authenticationListeners }
+        }
+
+        open class AuthenticationListeners {
+            open fun onAuthenticationError(errorCode: Int, errString: String) {}
+
+            open fun onAuthenticationSucceeded() {}
+
+            open fun onAuthenticationFailed() {}
+        }
+    }
+}
