56913f57604f3678663166dd738cc441ed71a855
rooney123777 <rooney123777@gmail.com>
2023-05-30 16:31:36 +0300

    version 1.2.2 release AppBarLayoutUtils offset change with animation added.

--- lightbulb-commons/src/main/java/com/github/RooneyAndShadows/lightbulb/commons/utils/AppBarLayoutUtils.kt
+++ lightbulb-commons/src/main/java/com/github/RooneyAndShadows/lightbulb/commons/utils/AppBarLayoutUtils.kt
@@ -1,18 +1,27 @@
 package com.github.rooneyandshadows.lightbulb.commons.utils
 
+import android.R.integer.config_shortAnimTime
+import android.animation.ValueAnimator
+import android.view.animation.DecelerateInterpolator
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.appbar.AppBarLayout
+import java.lang.Integer.max
 import java.util.*
 
 @Suppress("unused")
 class AppBarLayoutUtils {
     companion object {
         @JvmStatic
-        fun setAppBarLayoutOffset(appBarLayout: AppBarLayout, offset: Int) {
+        @JvmOverloads
+        fun setAppBarLayoutOffset(
+            appBarLayout: AppBarLayout,
+            offset: Int,
+            animate: Boolean = true,
+            duration: Int = ResourceUtils.getIntById(appBarLayout.context, config_shortAnimTime),
+        ) {
             if (appBarLayout.layoutParams !is CoordinatorLayout.LayoutParams) return
-            val param = appBarLayout.layoutParams as CoordinatorLayout.LayoutParams
-            val behavior = param.behavior as AppBarLayout.Behavior?
-            behavior?.let { it.topAndBottomOffset = offset }
+            if (animate) animateOffset(appBarLayout, offset, max(0, duration))
+            else setAppBarLayoutOffsetInternally(appBarLayout, offset)
         }
 
         @JvmStatic
@@ -21,6 +30,28 @@ class AppBarLayoutUtils {
             val param = appBarLayout.layoutParams as CoordinatorLayout.LayoutParams
             val behavior = param.behavior as AppBarLayout.Behavior?
             return behavior?.topAndBottomOffset ?: 0
+        }
+
+        @JvmStatic
+        private fun setAppBarLayoutOffsetInternally(appBarLayout: AppBarLayout, offset: Int) {
+            if (appBarLayout.layoutParams !is CoordinatorLayout.LayoutParams) return
+            val param = appBarLayout.layoutParams as CoordinatorLayout.LayoutParams
+            val behavior = param.behavior as AppBarLayout.Behavior?
+            behavior?.let { it.topAndBottomOffset = offset }
+        }
+
+        @JvmStatic
+        private fun animateOffset(appBarLayout: AppBarLayout, newOffset: Int, duration: Int) {
+            val valueAnimator = ValueAnimator.ofInt()
+            valueAnimator.interpolator = DecelerateInterpolator()
+            valueAnimator.addUpdateListener { animation ->
+                val newValue = (animation.animatedValue as Int)
+                setAppBarLayoutOffsetInternally(appBarLayout, newValue)
+                appBarLayout.requestLayout()
+            }
+            valueAnimator.setIntValues(getAppBarLayoutOffset(appBarLayout), newOffset)
+            valueAnimator.duration = duration.toLong()
+            valueAnimator.start()
         }
     }
 }
